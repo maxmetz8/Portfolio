22456a213db8a03d113db6609ccc6cbbbf965b9b
MaxMetz <maxmetz8@gmail.com>
2024-05-28 22:47:11 +0200

    yeah

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Huur Timo nu voor 5 euro per uur!
         </a>
       </header>
     </div>
